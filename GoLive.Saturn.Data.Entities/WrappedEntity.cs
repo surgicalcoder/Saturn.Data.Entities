151e44eb4bffcb9d68f0551e7e8dd2c14571e965
Monty <ma@surgicalcoder.com>
2020-05-04 21:01:49 +0100

    Implicit operators

--- GoLive.Saturn.Data.Entities/WrappedEntity.cs
+++ GoLive.Saturn.Data.Entities/WrappedEntity.cs
@@ -2,6 +2,17 @@
 {
     public class WrappedEntity<T> : Entity
     {
+        public static implicit operator T(WrappedEntity<T> someValue)
+        {
+            return someValue.Item;
+        }
+
+        public static implicit operator WrappedEntity<T>(T item)
+        {
+            return new WrappedEntity<T>() { Item = item };
+        }
+
+
         public T Item { get;set; }
     }
 }
