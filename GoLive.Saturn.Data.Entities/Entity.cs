968d328a224e515f0725c41473d6a1491ba88eab
Monty <ma@surgicalcoder.com>
2020-09-12 20:54:48 +0100

    Adding in properties

--- GoLive.Saturn.Data.Entities/Entity.cs
+++ GoLive.Saturn.Data.Entities/Entity.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GoLive.Saturn.Data.Entities
 {
@@ -6,6 +7,10 @@ namespace GoLive.Saturn.Data.Entities
     {
         public virtual string Id { get; set; }
         public virtual long? Version { get; set; }
+        public virtual Dictionary<string, dynamic> Properties { get; set; } = new Dictionary<string, object>();
+
+        protected Entity()
+        { }
 
         public bool Equals(Entity other)
         {
